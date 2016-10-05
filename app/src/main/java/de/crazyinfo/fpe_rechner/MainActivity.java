1b7cf936ee9e38e135108e61d6238271c63cebcd
Mathias Noack <mathias_noack@t-online.de>
2016-10-05 22:07:21 +0200

    Update MainActivity.java

--- app/src/main/java/de/crazyinfo/fpe_rechner/MainActivity.java
+++ app/src/main/java/de/crazyinfo/fpe_rechner/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends Activity
             double kcal;                // Kcal
             double factor;              // Faktor
             double insulin;             // Insulin
-            double fpu;                 // Fett-Protein-Einheiten
+            double fpu;                 // Fett-Protein-Einheiten gerundet
 
             /* Diese erhält den Wert des Feldes als Zahl (Kohlenhydrate & KCAL & Faktor) */
             cho = Float.valueOf(editTextCho.getText().toString());
@@ -78,70 +78,59 @@ public class MainActivity extends Activity
             factor = Float.valueOf(editTextFactor.getText().toString());
 
             /* Berechnungen */
-            cho = Math.abs(cho * 4);                                        // Kohlenhydrate * 4
-            fpu = (double) Math.round((kcal - cho) / 100 * 10 * 1) / 10;    // FPE-Gehalt auf eine Nachkommastelle gerundet
-            insulin = (double) Math.round((fpu * factor * 10) * 1) / 10;    // Insulinmenge auf eine Nachkommastelle gerundet
-
+            cho = Math.abs(cho * 4);                                                                // Kohlenhydrate * 4
+            fpu = (double) Math.round(((kcal - cho) / 100 * 10) * 1) / 10;                          // FPE-Gehalt auf eine Nachkommastelle gerundet
+            insulin = (double) Math.round(((kcal - cho) / 100 * factor * 10) * 1) / 10;             // Insulinmenge auf eine Nachkommastelle gerundet
 
             /*  Stundenvergleich für die Insulinabgabe */
             String[] arrayCheckHours = {"3", "4", "5", "7 - 8", "0"};
             String checkHours = "0";
 
-            if (kcal >= 100 && kcal < 200)
-            {
+            if (kcal >= 100 && kcal < 200) {
                 System.out.println(arrayCheckHours[0]);         // 3 Stunden
                 checkHours = arrayCheckHours[0].toString();
             }
-            if (kcal >= 200 && kcal < 300)
-            {
+            if (kcal >= 200 && kcal < 300) {
                 System.out.println(arrayCheckHours[1]);         // 4 Stunden
                 checkHours = arrayCheckHours[1].toString();
             }
-            if (kcal >= 300 && kcal < 400)
-            {
+            if (kcal >= 300 && kcal < 400) {
                 System.out.println(arrayCheckHours[2]);         // 5 Stunden
                 checkHours = arrayCheckHours[2].toString();
             }
-            if (kcal >= 400)
-            {
+            if (kcal >= 400) {
                 System.out.println(arrayCheckHours[3]);         // 7 - 8 Stunden
                 checkHours = arrayCheckHours[3].toString();
             }
-            if (insulin <= 0)
-            {
+            if (insulin <= 0) {
                 System.out.println(arrayCheckHours[4]);         // Wenn Insulin kleiner gleich 0, dann auch 0 Stunden
                 checkHours = arrayCheckHours[4].toString();
             }
-            if (fpu <= 0)
-            {
+            if (fpu <= 0) {
                 System.out.println(arrayCheckHours[4]);
                 checkHours = arrayCheckHours[4].toString();     // Wenn Fett-Protein-Einheiten kleiner gleich 0, dann auch 0 Stunden
             }
 
             /*  Insulinabgabe prüfen*/
             double[] arrayInsulin = {0};
-            if (insulin < 0)
-            {
+            if (insulin < 0) {
                 insulin = arrayInsulin[0];                      // Wenn Insulin kleiner 0, dann auch Insulin 0
             }
-            if (kcal < 100)
-            {
-                insulin = arrayInsulin[0];                      // Wenn FPU kleiner 0, dann auch FPE 0
+            if (kcal < 100) {
+                insulin = arrayInsulin[0];                      // Wenn Kcal kleiner 0, dann auch Insulin 0
             }
 
             /*  FPE-Gehalt prüfen*/
             double[] arrayFpu = {0};
-            if (fpu < 0)
-            {
+            if (fpu < 0) {
                 fpu = arrayFpu[0];                              // Wenn FPE kleiner 0, dann auch FPE 0
             }
-            if (insulin <= 0)
-            {
+            if (insulin <= 0) {
                 fpu = arrayFpu[0];                              // Wenn Insulin kleiner gleich 0, dann auch FPE 0
             }
 
             /* Ergebnisausgabe */
             textViewResult.setText(getString(R.string.calcResult) + fpu + getString(R.string.textFpu) + insulin + getString(R.string.amountOfInsulin) + checkHours + getString(R.string.hoursText));
-            }
         }
     }
+}
